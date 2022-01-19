6fc14034863265a63b88465833177ea18221ef56
Kamol-Paul <s1910176121@ru.ac.bd>
2022-01-19 08:50:23 +0600

    NewtonDivided  has been added.

--- src/Interpolation/InterpolationChecker.java
+++ src/Interpolation/InterpolationChecker.java
@@ -2,10 +2,15 @@ package Interpolation;
 
 public class InterpolationChecker {
     public static void main(String[] args) {
-        double[] X = {0,1,2,3};
-        double[] Y = {1,0,1,10};
-        NewtonForwardFormula newtonForwardFormula = new NewtonForwardFormula(X,Y);
-        System.out.println(newtonForwardFormula.F(0.5));
+//        double[] X = {0,1,2,3};
+//        double[] Y = {1,0,1,10};
+//        NewtonForwardFormula newtonForwardFormula = new NewtonForwardFormula(X,Y);
+//        System.out.println(newtonForwardFormula.F(0.5));
+        double X[] = {300,304,305,307};
+        double Y[] = {2.4771,2.4829,2.4843,2.4871};
+        NewtonDivided newtonDivided = new NewtonDivided(X,Y);
+        System.out.println(newtonDivided.F(301));
+
 
     }
 }
--- src/Interpolation/NewtonDivided.java
+++ src/Interpolation/NewtonDivided.java
@@ -1,30 +1,29 @@
 package Interpolation;
 
-public class NewtonForwardFormula {
+public class NewtonDivided {
     double[] X;
     double[] Y;
     double [][] delY;
     double []cof;
     int n;
-    double h;
-    NewtonForwardFormula(double []x, double[] y){
+    NewtonDivided(double[] x, double[] y){
         this.X = x;
         this.Y = y;
         this.n = this.X.length;
         delY = new double[n][n];
         cof = new double[this.n];
-        this.h = this.X[1]-this.X[0];
+
         for(int i=0;i<this.n;i++){
             for(int j=0;j<this.n-i;j++){
                 if(i==0) delY[j][0]=Y[j];
-                else delY[j][i] = delY[j+1][i-1]-delY[j][i-1];
-                //System.out.print(delY[i][j]+" ");
-            }
+                else delY[j][i] = (delY[j+1][i-1]-delY[j][i-1])/(X[j+i]-X[j]);
+                //if(i>0)System.out.print((delY[j+1][i-1])+" "+(delY[j][i-1])+" "+X[j+i]+" "+X[j]+" ");
+            }//
             //System.out.println("");
         }
 //        for(int i=0;i<this.n;i++){
 //            for(int j=0;j<this.n-i;j++){
-//                System.out.print(String.format("%5f",delY[i][j])+" ");
+//                System.out.print(String.format("%5.10f",delY[i][j])+" ");
 //            }
 //            System.out.println();
 //        }
@@ -32,13 +31,10 @@ public class NewtonForwardFormula {
 
     }
     private void getCofficient(){
-        double curH = 1;
-        double curFac = 1;
+
         cof[0] = Y[0];
         for(int i=1;i<this.n;i++){
-            curH*=h;
-            curFac *= i;
-            cof[i] = delY[0][i]/(curFac*curH);
+            cof[i] = delY[0][i];
         }
     }
     double F(double x){
@@ -51,5 +47,4 @@ public class NewtonForwardFormula {
         }
         return y;
     }
-
 }
